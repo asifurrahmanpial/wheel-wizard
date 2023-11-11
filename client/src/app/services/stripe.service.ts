3587f17c7914c4b71ce6e1f94518445935ebc570
Asifur Rahman <asifurrahmanpial@gmail.com>
2023-11-12 02:31:39 +0600

    fixed finally

--- client/src/app/services/stripe.service.ts
+++ client/src/app/services/stripe.service.ts
@@ -3,7 +3,6 @@ import { Stripe } from '@capacitor-community/stripe';
 import { HttpClient } from '@angular/common/http';
 import { first } from 'rxjs';
 import { environment } from 'src/environments/environment';
-
 @Injectable({
 	providedIn: 'root'
 })
@@ -13,27 +12,23 @@ export class StripeService {
 			publishableKey: environment.stripe.publishableKey
 		});
 	}
-
 	async createPaymentIntent(email: string, fare: number): Promise<string> {
 		const data = {
 			email,
 			fare
 		};
-
 		const response = await this.http
 			.post<any>(environment.api + 'payment', data)
 			.pipe(first())
 			.toPromise();
 		return response.client_secret;
 	}
-
 	async openPaymentSheet(clientSecret: string) {
 		try {
 			await Stripe.createPaymentSheet({
 				paymentIntentClientSecret: clientSecret,
 				merchantDisplayName: 'Technyks'
 			});
-
 			const result = await Stripe.presentPaymentSheet();
 			return result;
 		} catch (e) {
