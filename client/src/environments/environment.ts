b15f3296ed3a0610ee1661165808ca2bb100c8b8
Asifur Rahman <asifurrahmanpial@gmail.com>
2023-11-12 02:48:50 +0600

    finally fixed

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -11,5 +11,5 @@ export const environment = {
 		secretKey:
 			'sk_test_51O9ZJlEtvqqTQS33ExcAiFB3P6RZax0E6rjZN0BcEliL7VpicWRrB3GmPZxqB30eQRbpgPS11xO86xAkH9naCtMT00HeRaA8vP'
 	},
-	api: 'https://wheel-wizard-api.cyclic.app/'
+	api: 'http://192.168.0.120:3001/'
 };
